e6ca32779418a31a196eda81d0121c5f557ab39c
sky line <32512284+linesky@users.noreply.github.com>
2024-04-22 21:42:33 +0100

    Add files via upload

--- decompiler.py
+++ decompiler.py
@@ -9,8 +9,31 @@ def scommand(data):
 def sline(argvs):
      
      scommand("objdump  -M intel -d -S $1 > /tmp/out.txt".replace("$1",argvs))
-     scommand("gedit /tmp/out.txt")			
+     f1=open("/tmp/out.txt","r")
+     txt=f1.read()
+     f1.close()
+     txt=txt.replace("\r","\n")
+     txt=txt.replace("\n\n","\n")
      
+     txtx=txt.split("\n")
+     txt=None
+     inss1=None
+     inss=None
+     for n in txtx:
+         inss=n.find(">:")
+         if inss>-1:
+             n=n.replace(">","")
+             inss1=n.find("<")
+             if inss1>-1:
+                 inss1+=1
+                 n=n[inss1:]
+         
+         else:
+             inss1=n.find(":")
+             if inss1>-1:
+              	inss1+=1
+              	n=n[inss1:]		
+         print(n)
 	
 print("\x1bc\x1b[43;37m")	
 if sys.argv[1]!="":
