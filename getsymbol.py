a446609f6f6adb0fa2d854d7129859a54863b80f
sky line <32512284+linesky@users.noreply.github.com>
2024-04-23 02:02:48 +0100

    Add files via upload

---
+++ getsymbol.py
@@ -0,0 +1,61 @@
+import sys
+import os
+def srun(data):
+	os.system(data)
+		
+def scommand(data):
+	srun(data)
+	
+def sline(argvs1,argvs2):
+     
+     scommand('objdump  -M intel -d -S --disassemble=$2 "$1" > /tmp/out.txt'.replace("$1",argvs1).replace("$2",argvs2))
+     f1=open("/tmp/out.txt","r")
+     txt=f1.read()
+     f1.close()
+     txt=txt.replace("\r","\n")
+     txt=txt.replace("\n\n","\n")
+     
+     txtx=txt.split("\n")
+     txt=None
+     inss1=None
+     inss2=None
+     inss3=None
+     inss=None
+     for n in txtx:
+         inss=n.find(">:")
+         if inss>-1:
+             n=n.replace(">","")
+             inss1=n.find("<")
+             if inss1>-1:
+                 inss1+=1
+                 n=n[inss1:]
+                 print("")
+                 print(n)
+         else:
+             inss1=n.find(":")
+             
+             if inss1>-1:
+              	inss1+=1
+              	n=n[inss1:]
+
+              	xtxt=n.split("	")
+              	n1=0;
+              	
+              	if len(xtxt)>1:
+              	    print("        ",end="")
+              	    for nnn in xtxt:
+              	        if n1>1:
+              	            inss2=nnn.find(" <")
+              	            if inss2>-1:
+              	                inss3=nnn.find("   ")
+              	                nnn=nnn[:inss3]+nnn[inss2:]
+              	                nnn=nnn.replace("<","")
+              	                nnn=nnn.replace(">","")
+              	            print(nnn+" ",end="")		
+              	        n1+=1
+              	    print("")
+              	
+	
+print("\x1bc\x1b[43;37m")	
+if sys.argv[1]!="" and sys.argv[2]!="":
+	sline(sys.argv[1],sys.argv[2])
