829249b277acf309e93272442d9880c554d77034
sky line <32512284+linesky@users.noreply.github.com>
2024-04-23 01:49:41 +0100

    Add files via upload

---
+++ symbol.py
@@ -0,0 +1,39 @@
+import sys
+import os
+def srun(data):
+	os.system(data)
+		
+def scommand(data):
+	srun(data)
+	
+def sline(argvs):
+     
+     scommand("objdump  -M intel -d -S $1 > /tmp/out.txt".replace("$1",argvs))
+     f1=open("/tmp/out.txt","r")
+     txt=f1.read()
+     f1.close()
+     txt=txt.replace("\r","\n")
+     txt=txt.replace("\n\n","\n")
+     
+     txtx=txt.split("\n")
+     txt=None
+     inss1=None
+     inss2=None
+     inss3=None
+     inss=None
+     for n in txtx:
+         inss=n.find(">:")
+         if inss>-1:
+             n=n.replace(">","")
+             inss1=n.find("<")
+             if inss1>-1:
+                 inss1+=1
+                 n=n[inss1:]
+                 print("")
+                 print(n)
+ 
+              	
+	
+print("\x1bc\x1b[43;37m")	
+if sys.argv[1]!="":
+	sline(sys.argv[1])
