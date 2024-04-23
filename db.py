4e091a45d68d543cc8cced5fc309af27f8fabe00
sky line <32512284+linesky@users.noreply.github.com>
2024-04-23 02:27:39 +0100

    Add files via upload

---
+++ db.py
@@ -0,0 +1,53 @@
+import sys
+import os
+def srun(data):
+	os.system(data)
+		
+def scommand(data):
+	srun(data)
+	
+def sline(argvs1,argvs2):
+     
+     scommand('objdump  -M intel -d -S --disassemble=$2 "$1" > /tmp/out.txt'.replace("$1",argvs1).replace("$2",argvs2))
+     f1=open("/tmp/out.txt","r")
+     txt=f1.read()
+     f1.close()
+     txt=txt.replace("\r","\n")
+     txt=txt.replace("\n\n","\n")
+     
+     txtx=txt.split("\n")
+     txt=None
+     inss1=None
+     inss2=None
+     inss3=None
+     inss=None
+     for n in txtx:
+         inss=n.find(">:")
+         if inss>-1:
+             n=n.replace(">","")
+             inss1=n.find("<")
+             if inss1>-1:
+                 inss1+=1
+                 n=n[inss1:]
+                 print("")
+                 print(n)
+         else:
+             inss1=n.find(":")
+             
+             if inss1>-1:
+              	inss1+=1
+              	n=n[inss1:]
+
+              	xtxt=n.split("	")
+              	n1=0;
+              	
+              	if len(xtxt)>1:
+              	    print(xtxt[1])
+              	    
+
+              	   
+              	
+	
+print("\x1bc\x1b[43;37m")	
+if sys.argv[1]!="" and sys.argv[2]!="":
+	sline(sys.argv[1],sys.argv[2])
